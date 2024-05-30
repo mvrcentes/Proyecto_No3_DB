3a4d1d6affb2fdba8c64cbe4f5e6704f08e8c8ae
Samuel Argueta <arg211024@uvg.edu.gt>
2024-05-30 12:15:49 -0600

    funciona

--- HBase.py
+++ HBase.py
@@ -1,4 +1,5 @@
 from tabla import Tabla
+from file import File as HFile, Row
 import numpy as np
 import csv
 
@@ -21,27 +22,95 @@ class HBase:
 		data = np.array(data)
 		data = data[~np.all(data == '', axis=1)]
 		data = data[1:]
-		self.tables['Data'] = Tabla('movies', ['movie_details', 'rating'])
+		
+		self.tables['data'] = Tabla('movies', ['movie_details', 'rating'])
+		self.tables['data'].add_column('movie_details', 'id')
+		self.tables['data'].add_column('movie_details', 'title')
+		self.tables['data'].add_column('movie_details', 'year')
+		self.tables['data'].add_column('movie_details', 'genre')
+		self.tables['data'].add_column('movie_details', 'duration')
+		self.tables['data'].add_column('movie_details', 'country')
+
+		self.tables['data'].add_column('rating', 'id')
+		self.tables['data'].add_column('rating', 'title')
+		self.tables['data'].add_column('rating', 'director')
+		self.tables['data'].add_column('rating', 'rating')
 
 		count_movie_details = 1
 		for row in data:
-			self.Put('Data', str(count_movie_details), 'movie_details', 'id', row[0])
-			self.Put('Data', str(count_movie_details), 'movie_details', 'title', row[1])
-			self.Put('Data', str(count_movie_details), 'movie_details', 'year', row[2])
-			self.Put('Data', str(count_movie_details), 'movie_details', 'genre', row[3])
-			self.Put('Data', str(count_movie_details), 'movie_details', 'duration', row[5])
-			self.Put('Data', str(count_movie_details), 'movie_details', 'country', row[7])
+			self.Put('data', str(count_movie_details), 'movie_details', 'id', row[0])
+			self.Put('data', str(count_movie_details), 'movie_details', 'title', row[1])
+			self.Put('data', str(count_movie_details), 'movie_details', 'year', row[2])
+			self.Put('data', str(count_movie_details), 'movie_details', 'genre', row[3])
+			self.Put('data', str(count_movie_details), 'movie_details', 'duration', row[5])
+			self.Put('data', str(count_movie_details), 'movie_details', 'country', row[7])
 			count_movie_details += 1
 
 		count_rating = 1
 		for row in data:
-			self.Put('Data', str(count_rating), 'rating', 'id', row[0])
-			self.Put('Data', str(count_rating), 'rating', 'title', row[2])
-			self.Put('Data', str(count_rating), 'rating', 'director', row[4])
-			self.Put('Data', str(count_rating), 'rating', 'rating', row[6])
+			self.Put('data', str(count_rating), 'rating', 'id', row[0])
+			self.Put('data', str(count_rating), 'rating', 'title', row[2])
+			self.Put('data', str(count_rating), 'rating', 'director', row[4])
+			self.Put('data', str(count_rating), 'rating', 'rating', row[6])
 			count_rating += 1
-		print(f"data: {self.tables}")
+		
+		# print('Data Cargada Exitosamente!!!')
 	# id,título,año,género,director,duración,calificación,país
+
+
+	def Create_Test_Table(self):
+		self.tables['test'] = Tabla('users', ['general', 'address'])
+		self.tables['test'].add_column('general', 'name')
+		self.tables['test'].add_column('general', 'age')
+		self.tables['test'].add_column('address', 'street')
+		self.tables['test'].add_column('address', 'city')
+		hfile = HFile([
+		Row('1', 'general:name', '1', 'John'),
+		Row('1', 'general:age', '1', 20),
+		], 'general')
+		# For address info
+		hfile2 = HFile([
+				Row('1', 'address:street', '1', '123 Main St'),
+				Row('1', 'address:city', '1', 'New York'),
+		], 'address')
+		self.tables['test'].h_files.append(hfile)
+		self.tables['test'].h_files.append(hfile2)
+
+			
+	def Create_Test_Table2(self):
+		self.tables['test'] = Tabla('users', ['general', 'address'])
+		self.tables['test'].add_column('general', 'name')
+		self.tables['test'].add_column('general', 'age')
+		self.tables['test'].add_column('address', 'street')
+		self.tables['test'].add_column('address', 'city')
+
+		hfile = HFile([
+			Row(1, 'general:name', 1, 'John'),
+			Row(1, 'general:age', 1, 20),
+			Row(2, 'general:name', 1, 'Joshua'),
+			Row(2, 'general:age', 1, 25),
+			Row(3, 'general:name', 1, 'Sofia'),
+			Row(3, 'general:age', 1, 22),
+			Row(4, 'general:name', 1, 'Rose'),
+			Row(4, 'general:age', 1, 23),
+			Row(5, 'general:name', 1, 'Lily'),
+			Row(5, 'general:age', 1, 24),
+		], 'general')
+		# For address info
+		hfile2 = HFile([
+			Row(1, 'address:street', 1, '123 Main St'),
+			Row(1, 'address:city', 1, 'New York'),
+			Row(2, 'address:street', 1, '456 Main St'),
+			Row(2, 'address:city', 1, 'San Francisco'),
+			Row(3, 'address:street', 1, '789 Main St'),
+			Row(3, 'address:city', 1, 'Miami'),
+			Row(4, 'address:street', 1, '101 Main St'),
+			Row(4, 'address:city', 1, 'Los Angeles'),
+			Row(5, 'address:street', 1, '102 Main St'),
+			Row(5, 'address:city', 1, 'Chicago'),
+		], 'address')
+		self.tables['test'].h_files.append(hfile)
+		self.tables['test'].h_files.append(hfile2)
 	
 	def Create(self, name, familia_columnas):
 		"""
@@ -221,33 +290,91 @@ class HBase:
 		self.tables[nombre_tabla].describe()  # Describe la tabla
 		return True  # Retorna True indicando éxito
 
-	def Get(self, nombre_tabla, row_key, familia_columnas, version):
-		if nombre_tabla in self.tables.keys():
+	def Get(self, table_name, row_key, columns, version=1):
+		"""
+		Obtiene los datos de una tabla específica y una fila dada.
+
+		@param table_name: Nombre de la tabla.
+		@param row_key: Clave de la fila.
+		@param columns: Lista de columnas en formato "cf:col" (familia de columnas:columna).
+		@param version: Versión de los datos a obtener (por defecto es 1).
+		@return: Lista de filas encontradas o None si la tabla no existe.
+		"""
+		if table_name in self.tables.keys():
 			rows = []
-			for colmna in familia_columnas:
-				cf, col = colmna.split(':')
-				filas_encontradas = self.tables[nombre_tabla].get(row_key, cf, col, version)
-				if filas_encontradas:
-					rows.append(filas_encontradas)
+			for column in columns:
+				cf, col = column.split(":")
+				rows_found = self.tables[table_name].get(row_key, cf, col, version)
+				if rows_found is not None:
+					# Append rows_found list contents to rows
+					rows.extend(rows_found)
 			return rows
-		return False
-	
-	def Scan(self, nombre_tabla, row_start = None, row_stop = None, limit = None):
-		if nombre_tabla in self.tables.keys():
-			return self.tables[nombre_tabla].scan(row_start, row_stop, limit)
 		return None
+
+	def Delete(self, table_name, row_key=None, column_family=None, column_name=None, timestamp=None):
+		"""
+		Elimina datos de una tabla específica.
+
+		@param table_name: Nombre de la tabla.
+		@param row_key: Clave de la fila (opcional).
+		@param column_family: Familia de columnas (opcional).
+		@param column_name: Nombre de la columna (opcional).
+		@param timestamp: Timestamp de la eliminación (opcional).
+		@return: Resultado de la operación de eliminación o 0 si la tabla no existe.
+		"""
+		if table_name in self.tables.keys():
+			return self.tables[table_name].delete(row_key, column_family, column_name, timestamp)
+		return 0
+
 	
+	def Scan(self, table_name, row_start=None, row_stop=None, limit=None):
+		"""
+		Escanea una tabla en un rango de filas.
+
+		@param table_name: Nombre de la tabla.
+		@param row_start: Clave de la fila de inicio (opcional).
+		@param row_stop: Clave de la fila de fin (opcional).
+		@param limit: Límite de filas a escanear (opcional).
+		@return: Resultados del escaneo o None si la tabla no existe.
+		"""
+		if table_name in self.tables.keys():
+			return self.tables[table_name].scan(row_start, row_stop, limit)
+		return None
+
 	def Count(self, nombre_tabla):
+		"""
+		Cuenta el número de filas en una tabla.
+
+		@param nombre_tabla: Nombre de la tabla.
+		@return: Número de filas o False si la tabla no existe.
+		"""
 		if nombre_tabla in self.tables.keys():
 			return self.tables[nombre_tabla].count()
 		return False
-	
-	def Put(self, table_name, row_key, column_family, column_name, value, timestamp = None):
+
+	def Put(self, table_name, row_key, column_family, column_name, value, timestamp=None):
+		"""
+		Inserta un dato en una tabla.
+
+		@param table_name: Nombre de la tabla.
+		@param row_key: Clave de la fila.
+		@param column_family: Familia de columnas.
+		@param column_name: Nombre de la columna.
+		@param value: Valor a insertar.
+		@param timestamp: Timestamp de la inserción (opcional).
+		@return: Resultado de la operación de inserción o False si la tabla no existe.
+		"""
 		if table_name in self.tables.keys():
-			return self.tables[table_name].put(row_key, column_family, column_name, value, timestamp)
+			return self.tables[table_name].put(table_name, row_key, column_family, column_name, value, timestamp)
 		return False
-	
+
 	def Truncate(self, nombre_tabla):
+		"""
+		Trunca una tabla (elimina todos los datos).
+
+		@param nombre_tabla: Nombre de la tabla.
+		@return: True si la operación es exitosa, False si la tabla no existe.
+		"""
 		if nombre_tabla in self.tables.keys():
 			self.Disable(nombre_tabla)
 			self.tables[nombre_tabla].truncate()
